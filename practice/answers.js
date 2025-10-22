789623a8a6b8f265f12b750b3e41dc51c1526e72
matendecherej <matendecherej@gmail.com>
2025-10-22 23:59:45 +0300

    lessons learnt[A

---
+++ practice/answers.js
@@ -0,0 +1,6 @@
+console.log(19 % 3); //1
+console.log(10 == 3); //false
+console.log(10 !== "10"); //true
+console.log(2 < "10"); //true
+console.log("5" > 2); // true
+console.log((false && true) || true); //true
